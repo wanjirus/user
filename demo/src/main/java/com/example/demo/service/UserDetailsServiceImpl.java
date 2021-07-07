71f00aeef89a6072a1cb0ca01e2d2b49f77ae56a
USER <stanmburu254@gmail.com>
2021-07-07 06:50:59 -0700

    First commit

--- demo/src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -1,24 +1,16 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Roles;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.security.RolesAllowed;
-import javax.management.relation.Role;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
-
-import static java.util.Objects.*;
 
 @Service
 class UserDetailsServiceImpl implements UserDetailsService {
@@ -35,8 +27,10 @@ class UserDetailsServiceImpl implements UserDetailsService {
         User user = userRepository.findByUsername(username);
 
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-        for (Roles role : user.getRoles()) grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+       /* for (Roles role : user.getRoles())
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
 
+*/
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
     }
 }
