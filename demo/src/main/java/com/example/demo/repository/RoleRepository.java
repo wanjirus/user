01cd79116a64c7518fcaaf5de75cd062efe256de
USER <stanmburu254@gmail.com>
2021-07-08 10:27:45 -0700

    changes

--- demo/src/main/java/com/example/demo/repository/RoleRepository.java
+++ demo/src/main/java/com/example/demo/repository/RoleRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Roles;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface RoleRepository extends JpaRepository<Roles, Long>{
+    Roles findByUsers(Set<User> users);
 }
